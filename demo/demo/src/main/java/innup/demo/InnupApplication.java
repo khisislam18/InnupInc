477abcc4fa70590ad35e2d4028687dfea036ae40
khisislam18 <khisislam18@gmail.com>
2023-03-16 15:01:48 +0300

    -m "start project"

---
+++ demo/demo/src/main/java/innup/demo/InnupApplication.java
@@ -0,0 +1,13 @@
+package innup.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnupApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/innup/demo/InnupApplicationTests.java
@@ -0,0 +1,13 @@
+package innup.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
