1bf42d3698822700ca6b93bf3c2f008b0b4981ef
khisislam18 <khisislam18@gmail.com>
2023-03-31 15:21:49 +0300

    commit -m "add Hello world!"

---
+++ startupinvest/src/test/java/com/innup/startupinvest/StartupinvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innup.startupinvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupinvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
